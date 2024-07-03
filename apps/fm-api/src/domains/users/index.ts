bdb7ce80e710f84d175bac60fbbc26e80487ec88
Gabe Leon <gableonn@gmail.com>
2024-07-03 17:46:05 -0300

    try fix #1

--- apps/fm-api/src/domains/users/index.ts
+++ apps/fm-api/src/domains/users/index.ts
@@ -2,6 +2,6 @@ import { Application } from "express";
 import { signinHandler, singupHandler } from "./user.controller";
 
 export function setupUsersController(app: Application) {
-  app.use("/api/v1/auth/signup", singupHandler);
-  app.use("/api/v1/auth/signin", signinHandler);
+  app.post("/api/v1/auth/signup", singupHandler);
+  app.post("/api/v1/auth/signin", signinHandler);
 }
